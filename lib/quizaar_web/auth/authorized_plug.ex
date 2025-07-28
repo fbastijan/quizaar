2595e2b852011551058fb8a97011635f7cc73a6b
fbastijan <fbastijan@unipu.hr>
2025-07-28 15:11:14 +0200

    added authorization check for questions

--- lib/quizaar_web/auth/authorized_plug.ex
+++ lib/quizaar_web/auth/authorized_plug.ex
@@ -36,4 +36,29 @@ defmodule QuizaarWeb.Auth.AuthorizedPlug do
       raise QuizaarWeb.Auth.ErrorResponse.Forbidden
     end
   end
+
+  def is_authorized(%{params: %{"id" => id, "question" => question_params}} = conn, _opts) do
+    quiz = Quizaar.Quizzes.get_quiz!(question_params["quiz_id"])
+
+    if conn.assigns.account.user.id == quiz.user_id do
+      conn
+    else
+      IO.inspect(quiz.user_id, label: "Unauthorized access to quiz")
+      IO.inspect(conn.assigns.account.user.id, label: "Current user ID")
+      raise QuizaarWeb.Auth.ErrorResponse.Forbidden
+    end
+  end
+
+  def is_authorized(%Plug.Conn{private: %{phoenix_action: :delete, phoenix_controller: QuizaarWeb.QuestionController}, params: %{"id" => id}} = conn, _opts) do
+  # Your authorization logic for deleting a question goes here.
+  # Example: Only allow if the user owns the quiz this question belongs to.
+  question = Quizaar.Quizzes.get_question!(id)
+  quiz = Quizaar.Quizzes.get_quiz!(question.quiz_id)
+
+  if conn.assigns.account.user.id == quiz.user_id do
+    conn
+  else
+    raise QuizaarWeb.Auth.ErrorResponse.Forbidden
+  end
+end
 end
